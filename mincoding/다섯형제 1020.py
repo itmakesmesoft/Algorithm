39d2695288881ca9529b240c3be0b8e9ce8109cf
itmakesmesoft <dmsgur7112@gmail.com>
2022-08-24 00:01:04 +0900

    TIL

--- mincoding/다섯형제 1020.py
+++ mincoding/다섯형제 1020.py
@@ -1,15 +1,17 @@
 arr = list(map(int, input().split()))
 
-def dfs(level, total):
+def dfs(level, total, pastidx):
     global cnt
-    if total >= 10 and total <= 20:
+    if total>20:
+        return
+    if total >= 10:
         cnt+=1
-        return
-    elif level == 5:
-        return
-    
-    dfs(level+1, total+arr[level])
-    dfs(level+1, total)
+    for i in range(pastidx, 5):
+        if used[i]==0:
+            used[i]=1
+            dfs(level+1, total + arr[i], i)
+            used[i]=0
+used = [0]*5
 cnt = 0
-dfs(0, 0)
+dfs(0, 0, 0)
 print(cnt)
