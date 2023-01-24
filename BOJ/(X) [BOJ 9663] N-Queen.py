b77379ada7280bc5d6d877b6d4ec2d4d05b307bf
Daniel LEE <dmsgur7112@gmail.com>
2023-01-24 17:31:48 +0900

    TIL

---
+++ BOJ/(X) [BOJ 9663] N-Queen.py
@@ -0,0 +1,29 @@
+import sys
+
+def check_queen(level, i):
+    direct = [-1, 0, 1]
+    for j in range(level):
+        for k in range(3):
+            nx = i+direct[k]*(level-j)
+            if nx>N-1 or nx<0: continue
+            if MAP[j][nx]==1: return False
+    return True
+
+def dfs(level):
+    global count, visited, MAP
+    if level == N:
+        count += 1
+        return
+
+    for i in range(N):
+        if check_queen(level, i):
+            MAP[level][i]=1
+            dfs(level+1)
+            MAP[level][i]=0
+
+input = sys.stdin.readline
+N = int(input())
+MAP = [[0]*N for _ in range(N)]
+count = 0
+dfs(0)
+print(count)
